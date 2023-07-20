a7eaf71864cc8d61b33025162895ae97b1286446
arwaabdelrahem <arwaabdelrahem22@gmail.com>
2023-07-20 15:22:43 +0300

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,37 @@
+// module.exports = {
+//   apps: [
+//     {
+//       name: 'online-judge-microservice',
+//       script: './dist/main.js',
+//       instances: '2',
+//       exec_mode: 'cluster',
+//       autorestart: true,
+//       watch: '.',
+
+//     },
+//   ],
+//   deploy: {
+//     production: {
+//       // key: '.ssh/id_rsa.pub',
+//       user: 'root',
+//       host: ['146.190.170.90'],
+//       ref: 'origin/master',
+//       repo: 'https://github.com/arwaabdelrahem/online-judge-microservice',
+//       path: '/var/www/repositories',
+//       'post-deploy':
+//         'npm install && pm2 reload ecosystem.config.js --env production',
+//     },
+//   },
+// };
+
 module.exports = {
   apps: [
     {
-      name: 'online-judge-microservice',
+      name: 'Micro-service',
       script: './dist/main.js',
       instances: '2',
       exec_mode: 'cluster',
       autorestart: true,
-      watch: '.',
       env_production: {
         NODE_ENV: 'production',
         DB: 'mongodb+srv://Arwaabdelrahem:AOlKUPBeQqrPHpOr@cluster1.bgufy1w.mongodb.net/flowers?retryWrites=true&w=majority',
@@ -19,14 +44,14 @@ module.exports = {
   ],
   deploy: {
     production: {
-      // key: '.ssh/id_rsa.pub',
+      // "key"  : ".ssh/creds.pem",
       user: 'root',
       host: ['146.190.170.90'],
       ref: 'origin/master',
       repo: 'https://github.com/arwaabdelrahem/online-judge-microservice',
       path: '/var/www/repositories',
       'post-deploy':
-        'npm install && pm2 reload ecosystem.config.js --env production',
+        'npm install && npm run build &&  pm2 reload ecosystem.config.js --env production && pm2 save',
     },
   },
 };
